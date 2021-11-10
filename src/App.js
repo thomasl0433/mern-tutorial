161f0db5c4e5f2becdf6037b02de3a1b19930527
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2021-11-10 18:29:22 -0500

    almost done with create.js render

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Create from "./components/create";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Create />
+            </div>
+        );
+    }
+}
