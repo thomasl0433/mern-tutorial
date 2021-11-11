8d970719f586bd1fcaba9970ecd591cb04b9568e
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2021-11-11 09:53:12 -0500

    cren

--- src/components/create.js
+++ src/components/create.js
@@ -69,24 +69,66 @@ export default class Create extends Component {
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>Name of the person: </label>
-            <input 
-            type="text"
-            className="form-control"
-            value={this.state.person_name}
-            onChange={this.onChangePersonName}
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.person_name}
+              onChange={this.onChangePersonName}
             />
           </div>
           <div className="form-group">
             <label>Person's position: </label>
-            <input 
-            type="text"
-            className="form-control"
-            value={this.state.person_position}
-            onChange={this.onChangePersonPosition}    
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.person_position}
+              onChange={this.onChangePersonPosition}
             />
           </div>
           <div className="form-group">
-
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityLow"
+                value="Intern"
+                checked={this.state.person_level === "Intern"}
+                onChange={this.onChangePersonLevel}
+              />
+              <label className="form-check-label">Intern</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityMedium"
+                value="Junior"
+                checked={this.state.person_level === "Junior"}
+                onChange={this.onChangePersonLevel}
+              />
+              <label className="form-check-label">Junior</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityHigh"
+                value="Senior"
+                checked={this.state.person_level === "Senior"}
+                onChange={this.onChangePersonLevel}
+              />
+              <label className="form-check-label">Senior</label>
+            </div>
+          </div>
+          <div className="form-group">
+              <input 
+                type="submit"
+                value="Create person"
+                className="btn btn-primary"
+              />
           </div>
         </form>
       </div>
